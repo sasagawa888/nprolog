844d623b88de37e1dc55b47cdc9d8a4a6b2db4cf
Neil Hoskins <neil.hosk@pm.me>
2020-09-24 13:29:09 +0930

    Reworked the makefile so that it can be modified more easily, moved tests to a separate folder to avoid confusion

---
+++ tests/paip.pl
@@ -0,0 +1,20 @@
+%from PAIP example.
+
+likes(kim,robin).
+likes(sandy,lee).
+likes(sandy,kim).
+likes(robin,cats).
+likes(sandy,X) :- likes(X,cats).
+likes(kim,X) :- likes(X,lee),likes(X,kim).
+likes(X,X).
+
+/*
+?- likes(sandy,Who).
+Who = lee;
+Who = kim;
+Who = robin;
+Who = sandy;
+Who = cats;
+Who = sandy;
+false
+*/
