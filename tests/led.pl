6e9855384985d274a01370a86fd8942fe1c02b2b
sasagawa888 <ken-s@tkcnf.or.jp>
2020-10-19 17:08:53 +0900

    Added test code for wiringPI tests folder

---
+++ tests/led.pl
@@ -0,0 +1,12 @@
+setup :-
+    not(flag),wiringpi_setup_gpio(X),assert(flag),
+    pin_mode(5,output).
+
+test(0).
+test(N) :-
+    digital_write(5,1),
+    delay(1000),
+    digital_write(5,0),
+    delay(1000),
+    N1 is N - 1,
+    test(N1).
