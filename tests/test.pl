a1f9142e0b687f29e544ca2501fb0994a78fb30d
sasagawa888 <ken-s@tkcnf.or.jp>
2020-11-01 08:03:28 +0900

    Modify manual

--- tests/test.pl
+++ tests/test.pl
@@ -19,10 +19,6 @@ bar(N) :-
 my_member(X, [X | Ls]).
 my_member(X, [Y | Ls]) :- my_member(X, Ls).
 
-myappend([],L,L) :- !.
-myappend([A|L],B, [A|C]):-
-	myappend(L,B,C).
-
 intersection([],Y,[]).
 intersection([X|Xs],Y,[X|Z]) :-
     member(X,Y),
@@ -37,21 +33,72 @@ union([X|Xs],Y,Z) :-
 union([X|Xs],Y,[X|Z]) :-
     union(Xs,Y,Z).
 
+square(X,Y) :- Y is X*X.
 
-/* quick sort OK */
-/* 農夫問題　答えがNO */
-/* 経路の探索（２）　ok*/
-/* 積み木の移動　OK*/
-/* ライツアウト　リソースエラー*/
+first([X | Ys], X).
+rest([X | Ys], Ys).
+add_to_list(X, Ls, [X | Ls]).
 
-test(N) :-
-    ifthenelse(odd(N),write(odd_number),write(even_numver)).
+retrieve(1,  [X | Ls], X).
+retrieve(N, [Y | Ls], X) :-
+    N > 1, N1 is N - 1, retrieve(N1, Ls, X).
 
-test1(N) :-
-    odd(N)->write(odd_number);write(even_number).
+insert_at(1, X, Ls, [X | Ls]).
+insert_at(N, X, [Y | Ls], [Y | Zs]) :-
+    N > 1, N1 is N - 1, insert_at(N1, X, Ls, Zs).
+
+remove_at(1, [X | Ls], Ls).
+remove_at(N, [X | Ls], [X | Zs]) :-
+    N > 0, N1 is N - 1, remove_at(N1, Ls, Zs). 
+
+my_length([], 0).
+my_length([X | Xs], N) :- my_length(Xs, N1), N is N1 + 1.
+
+my_append([], Xs, Xs).
+my_append([X | Ls], Ys, [X | Zs]) :- my_append(Ls, Ys, Zs).
+
+my_reverse([], []).
+my_reverse([X | Xs], Ys) :-
+    my_reverse(Xs, Zs), append(Zs, [X], Ys).
+
+my_select(X, [X | Xs], Xs).
+my_select(X, [Y | Ys], [Y | Zs]) :- my_select(X, Ys, Zs).
+
+selects([], Ys).
+selects([X | Xs], Ys) :- my_select(X, Ys, Ys1), selects(Xs, Ys1).
 
 
-odd(N) :- X is N mod 2,X \= 0.
+my_flatten([X | Xs], Ys) :-
+    my_flatten(X, Ys1), my_flatten(Xs, Ys2), append(Ys1, Ys2, Ys).
+my_flatten(X, [X]) :- atomic(X), X \== [].
+my_flatten([], []).
+
+/*
+written by M.Hiroi
+thanks
+
+?- solve.
+*/
+set_op(N, Exp, [N, + | Exp]).
+set_op(N, Exp, [N, - | Exp]).
+set_op(N, [N1 | Rest], [N2 | Rest]) :- N2 is N1 * 10 + N.
+
+make_exp(10, Exp) :-
+    reverse(Exp, Exp1), calc_exp(Exp1, N), N =:= 100, write(Exp1), nl, fail.
+
+make_exp(N, Exp):-
+    N < 10, set_op(N, Exp, Exp1), N1 is N + 1, make_exp(N1, Exp1).
+
+calc_one_exp(A, +, B, N):- N is A + B.
+calc_one_exp(A, -, B, N):- N is A - B.
+
+calc_exp([Num], Num).
+calc_exp([A, Op, B | Rest], Num) :-
+    calc_one_exp(A, Op, B, Num1), calc_exp([Num1 | Rest], Num).
+
+solve :- make_exp(2, [1]).
+
+
 
 /*
 written by M.Hiroi
