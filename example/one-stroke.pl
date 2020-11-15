af6441d55959289a3bdfb004dbb31079c3e8c26a
sasagawa888 <ken-s@tkcnf.or.jp>
2020-11-15 14:19:33 +0900

    Modify one-stroke.pl

--- example/one-stroke.pl
+++ example/one-stroke.pl
@@ -24,14 +24,14 @@ solve([N|Ns]) :-
 solve1(X,[],R) :- reverse(R,R1),write(R1),nl,fail.
 solve1(X,E,R) :-
   root(X,Y,E1),
-  delete(E,E1,E2),
+  delete1(E,E1,E2),
   solve1(Y,E2,[Y|R]).
 
 solve1(X,E,R) :-
   root(Y,X,E1),
-  delete(E,E1,E2),
+  delete1(E,E1,E2),
   solve1(Y,E2,[Y|R]).
 
-delete([X|Xs],X,Xs).
-delete([X|Xs],Y,[X|Z]) :-
-  delete(Xs,Y,Z).
+delete1([X|Xs],X,Xs).
+delete1([X|Xs],Y,[X|Z]) :-
+  delete1(Xs,Y,Z).
