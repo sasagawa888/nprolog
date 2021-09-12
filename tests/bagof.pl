5a85796a289b352779070adf71a2bdcf987b0279
sasagawa888 <ken-s@tkcnf.or.jp>
2021-09-12 10:59:30 +0900

    Modified bagof.pl

--- tests/bagof.pl
+++ tests/bagof.pl
@@ -19,3 +19,20 @@ Bag = [fido, harry, harry] ;
 false.
 
 */
+
+foo(a, b, c).
+foo(a, b, d).
+foo(b, c, e).
+foo(b, c, f).
+foo(c, c, g).
+
+/*
+?- bagof(C, foo(A, B, C), Cs).
+A = a, B = b, C = G308, Cs = [c, d] ;
+A = b, B = c, C = G308, Cs = [e, f] ;
+A = c, B = c, C = G308, Cs = [g].
+
+?- bagof(C, A^foo(A, B, C), Cs).
+A = G324, B = b, C = G326, Cs = [c, d] ;
+A = G324, B = c, C = G326, Cs = [e, f, g].
+*/
