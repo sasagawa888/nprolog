6ca48ed08cf4ef0ccfa87347bb56b83a15dc9745
sasagawa888 <ken-s@tkcnf.or.jp>
2021-09-28 08:21:34 +0900

    Added example/turing.pl (Turing machine)

---
+++ example/turing.pl
@@ -0,0 +1,80 @@
+/*
+Turing machine in prolog
+from Kaname Yoshida' book
+
+tm(State,Position,Tape).
+state: q0.q1...qN)
+
+?- tm(q0,2,[0,1,1,1,0,1,1,1,1,0]).
+this calculates 2+3=5
+*/
+
+tm(q0,P,T) :-
+    nth(P,T,1),
+    state(q0,P,T),
+    P1 is P+1,
+    tm(q0,P1,T).
+tm(q0,P,T) :-
+    nth(P,T,0),
+    state(q0,P,T),
+    set(P,T,1,T1),
+    tm(q1,P,T1).
+tm(q1,P,T) :-
+    nth(P,T,1),
+    state(q1,P,T),
+    P1 is P-1,
+    tm(q1,P1,T).
+tm(q1,P,T) :-
+    nth(P,T,0),
+    state(q1,P,T),
+    P1 is P+1,
+    tm(q2,P1,T).
+tm(q2,P,T) :-
+    nth(P,T,1),
+    state(q2,P,T),
+    set(P,T,0,T1),
+    tm(q3,P,T1).
+tm(q3,P,T) :-
+    nth(P,T,0),
+    state(q3,P,T),
+    P1 is P+1,
+    tm(q4,P1,T).
+tm(q4,P,T) :-
+    nth(P,T,1),
+    state(q4,P,T),
+    set(P,T,0,T1),
+    tm(q5,P,T1).
+tm(q5,P,T) :-
+    nth(P,T,0),
+    state(q5,P,T),
+    P1 is P+1,
+    tm(qf,P1,T).
+tm(qf,P,T) :-
+    state(qf,P,T).
+
+
+
+nth(1,[X|_],X).
+nth(N,[_|Xs],Y) :-
+    N>1,
+    N1 is N-1,
+    nth(N1,Xs,Y).
+
+set(1,[_|Ls],X,[X|Ls]).
+set(N,[L|Ls],X,[L|Y]) :-
+    N>1,
+    N1 is N-1,
+    set(N1,Ls,X,Y).
+
+state(S,P,T) :-
+    space(P),
+    write(S),nl,
+    write(T),nl.
+    
+
+space(1).
+space(N) :-
+    N>1,
+    write('  '),
+    N1 is N-1,
+    space(N1).
