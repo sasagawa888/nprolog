79d68f619504e2e3fbe5335c8a49dad7005daa9d
sasagawa888 <ken-s@tkcnf.or.jp>
2022-12-04 09:00:05 +0900

    Debugging mworld.pl

--- tests/mworld.pl
+++ tests/mworld.pl
@@ -112,3 +112,16 @@ call_with(X,L) :-
     Y \= true,
     call_with(Y,L).
 
+deny(dummy).
+
+%example1
+:- with(a,assertz(p(a))).
+:- with(b,assertz(p(b))).
+:- with(c,assertz(p(c1))).
+:- with(c,assertz(p(c2))).
+
+%example2
+:- with(w1,assertz(bird(canary))).
+:- with(w1,assertz(bird(penguin))).
+:- with(w2,assertz((fly(X) :- bird(X)))).
+:- with(w3,deny(fly(penguin))).
