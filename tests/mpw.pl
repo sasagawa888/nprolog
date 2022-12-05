ab908fc0c8ebc415b20e34abd8a9232ec6c57bad
sasagawa888 <ken-s@tkcnf.or.jp>
2022-12-05 15:01:09 +0900

    Update version number

--- tests/mpw.pl
+++ tests/mpw.pl
@@ -1,4 +1,4 @@
-
+% World multiplex mechanism
 %example1
 :- with(a,assertz(p(a))).
 :- with(b,assertz(p(b))).
@@ -6,8 +6,8 @@
 :- with(c,assertz(p(c2))).
 
 %example2
-% with(w1,with(w2,with(w3,fly(canary)))) -> yes
-% with(w1,with(w2,with(w3,fly(penguin)))) -> no
+% with(w1,with(w2,with(w3,fly(canary)))). -> yes
+% with(w1,with(w2,with(w3,fly(penguin)))). -> no
 :- with(w1,assertz(bird(canary))).
 :- with(w1,assertz(bird(penguin))).
 :- with(w2,assertz((fly(X) :- bird(X)))).
