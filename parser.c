337caa8dfdd3f8a9a9a24db8a646a8aadd3e82bb
sasagawa888 <ken-s@tkcnf.or.jp>
2024-10-13 17:21:20 +0900

    Add comment for break/0

--- parser.c
+++ parser.c
@@ -1435,6 +1435,10 @@ int readitem1(void)
 	SET_CAR(temp, UNBIND);	//value
 	SET_CDR(temp, UNBIND);	//alpha variable
 	} else if(break_flag && !memberp(temp,variables)){
+		/* In the execution of break/0, 
+		 * the unification of the variables up to that point must be maintained. 
+		 * However, the values of the new variables generated in break/0 should not be retained.
+		*/
 		SET_CAR(temp, UNBIND);	//value
 		SET_CDR(temp, UNBIND);	//alpha variable
 	}
