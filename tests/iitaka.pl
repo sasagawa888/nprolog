21a6b035b759b61c97160abb360a777433be243f
sasagawa888 <ken-s@tkcnf.or.jp>
2024-10-19 12:19:39 +0900

    Add code iitaka.pl

--- tests/iitaka.pl
+++ tests/iitaka.pl
@@ -53,3 +53,35 @@ set(L==M) :-
 Y isl [] + Y :- !.
 Z isl [A|X] + Y :- member(A,Y),!,Z isl X + Y.
 [A|Z] isl [A|X] + Y :- Z isl X + Y.
+
+%p131
+memberr([A],X,1) :- member(A,X).
+memberr([A|LA],[A|Y],R) :-
+        R>1,R1 is R-1,memberr(LA,Y,R1).
+memberr(A,[_|Y],R) :-
+        R>1,memberr(A,Y,R).
+        
+/* generate [1,2,3,4,...] */
+generate_e(N,E) :- gene_e_aux(E,N,[]).
+gene_e_aux(L,0,L) :- !.
+gene_e_aux(Result,N,L) :-
+            N1 is N-1,
+            gene_e_aux(Result,N1,[N|L]).
+            
+/* problem */
+
+
+comb(N,R) :- generate_e(N,X),
+             memberr(A,X,R),
+             write(A),nl,fail.
+    
+    
+permutation([],[]).
+permutation([A|X1],Y) :-
+             delete0(Y1 = Y - A),
+             permutation(X1,Y1).
+      
+delete0(X = [A|X]-A).
+delete0([B|Y] = [B|X] - A) :- 
+             delete0(Y = X - A).
+             
