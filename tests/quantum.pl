83246a6548adb0f327addcd007d41b8c76c850a8
sasagawa888 <ken-s@tkcnf.or.jp>
2024-10-20 15:28:00 +0900

    Modify quantum.pl

--- tests/quantum.pl
+++ tests/quantum.pl
@@ -1,3 +1,5 @@
+/* quantum-number iitaka p41*/
+
 :- op(700,xfx,isq).
 
 
@@ -21,3 +23,18 @@ X isq q(R1,I1,J1,K1)*q(R2,I2,J2,K2) :-
     J is R1*J2 - I1*K2 + J1*R2 + K1*I2,
     K is R1*K2 + J1*K2 - K1*I2 + K1*R2,
     X= q(R,I,J,K).
+
+X isq q(R1,I1,J1,K1)/q(R2,I2,J2,K2) :-
+    I3 is -I1,
+    J3 is -J1,
+    K3 is -K1,
+    q(R4,I4,J4,K4) isq q(R1,I1,J1,K1)/q(R2,I2,J2,K2), 
+    D is (R1^2) + (I1^2) + (J1^2) + (K1^2),
+    R is R4/D,
+    I is I4/D,
+    J is J4/D,
+    K is K4/D,
+    X = q(R,I,J,K).
+
+X isq conjugate(q(R,I,J,K)) :-
+    X = q(R,-I,-J,-K).
