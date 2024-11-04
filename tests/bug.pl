111f60072aee434c9cfc31eebf44d9f42fd2bcda
sasagawa888 <ken-s@tkcnf.or.jp>
2024-11-04 15:07:30 +0900

    Debugging compiler

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,23 +1,10 @@
+foo(X) :- write(X),!,write(X).
 
-foo(X) :- bar(X,Y),write(Y).
+max(X,Y,X) :- X>=Y,!.
+max(X,Y,Y).
 
-bar(X) :- fail;fail;true.
-
-boo(X) :- write(X),!,write(X).
-
-a :- write('Disjunction test: a succeeded').
-b :- write('Disjunction test: b succeeded').
-or_test :- a ; b.
-
-x :- write('Cut test: x succeeded'), nl.
-y :- write('Cut test: y succeeded'), nl, !.
-z :- write('Cut test: z succeeded'), nl.
-
-cut_test :- x, (y; z).
-
-
-fact(0,1).
-fact(N,X) :-
-    N1 is N-1,
-    fact(N1,X1),
-    X is N*X1.
+fact(0,1) :- !.
+fact(X,Y) :- 
+    X1 is X-1,
+    fact(X1,Y1),
+    Y is X*Y1.
