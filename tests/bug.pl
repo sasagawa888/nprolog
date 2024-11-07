e8c1cc1ee6e8a353f22dee04e3e67981fbe877ac
sasagawa888 <ken-s@tkcnf.or.jp>
2024-11-07 17:47:58 +0900

    next goal is repeat_for. qsort and nreverse compile is OK.

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,7 +1,15 @@
+% Repeat a procedure
+repeat_for(0) :- !, fail.
+repeat_for(N).
+repeat_for(N) :-
+    M is N - 1,
+    repeat_for(M).
+
+/*
 max(X,Y,X) :- X>=Y,!.
 max(X,Y,Y).
 
-/*
+
 % Partition list for quicksort
 partition([X|L], Y, [X|L1], L2) :-
     X < Y, !, partition(L, Y, L1, L2).
