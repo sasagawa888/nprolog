3fe2bd765347acb2ef6cb18a51926e577db4e3ed
sasagawa888 <ken-s@tkcnf.or.jp>
2024-11-10 16:34:01 +0900

    Debugging disjunction in compiler

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,37 +1,4 @@
-% Repeat a procedure
-repeat_for(0) :- !, fail.
-repeat_for(N).
-repeat_for(N) :-
-    M is N - 1,
-    repeat_for(M).
-
-
-max(X,Y,X) :- X>=Y,!.
-max(X,Y,Y).
-
-
-% Partition list for quicksort
-partition([X|L], Y, [X|L1], L2) :-
-    X < Y, !, partition(L, Y, L1, L2).
-partition([X|L], Y, L1, [X|L2]) :-
-    !,partition(L, Y, L1, L2).
-partition([], _ , [], []) :- !.
-
-
-foo(X) :- write(X),!,write(X).
-
-bar :- fail;true.
-
-boo :- fail;fail;true.
-
-
-fact(0,1) :- !.
-fact(X,Y) :- 
-    X1 is X-1,
-    fact(X1,Y1),
-    Y is X*Y1.
-
-qdelete(A, A, L, L).
-qdelete(X, A, [H|T], [A|R]) :-
-    qdelete(X, H, T, R).
+test(X) :-
+    X = 1;   % First choice
+    X = 2.   % Second choice
 
