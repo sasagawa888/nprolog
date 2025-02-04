b6685dbbf8b05b1b2bc1907d1cab99965387ccef
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-04 17:55:33 +0900

    idea memo dynamic/1

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,5 +1,105 @@
-% test of module system
+%compile dynamic idea memo
 
-:- use_module(list).
+foo(X) :- boo(X).
+foo(X) :- bar(X).
 
-foo(X,Y) :- last(X,Y).
+test :- 
+    n_clause_with_arity(foo,1,C),
+    n_variable_convert(C,X),
+    jump_gen_dynamics(X).
+
+jump_gen_dynamics([]).
+jump_gen_dynamics([X|Xs]) :-
+    jump_gen_dynamic(X),nl,
+    jump_gen_dynamics(Xs).
+
+jump_gen_dynamic([]).
+jump_gen_dynamic((X :- Y)) :-
+    write('Jlist3(Jmakesys(":-"),'),
+    jump_gen_dynamic(X),
+    write(','),
+    jump_gen_dynamic(Y),
+    write(');').
+
+jump_gen_dynamic((X,Y)) :-
+    write('Jlist3(Jmakesys(","),'),
+    jump_gen_dynamic(X),
+    write(','),
+    jump_gen_dynamic(Y),
+    write(');').
+
+jump_gen_dynamic((X;Y)) :-
+    write('Jlist3(Jmakesys(";")),'),
+    jump_gen_dynamic(X),
+    write(','),
+    jump_gen_dynamic(Y),
+    write(');').
+    
+jump_gen_dynamic(X) :-
+    n_property(X,predicate),
+    X =.. [P|L],
+    write('Jcons(Jmakepred("'),
+    write(P),
+    write('"),'),
+    jump_gen_dynamic_argument(L),
+    write(',th)').
+
+jump_gen_dynamic(X) :-
+    n_property(X,builtin),
+    functor(X,P,0),
+    n_findatom(P,builtin,A),
+    write(A).
+jump_gen_dynamic(X) :-
+    n_property(X,builtin),
+    X =.. [P|L],
+    n_findatom(P,builtin,A),
+    write('Jcons('),
+    write(A),
+    write(','),
+    jump_gen_dynamic_argument(L),
+    write(',th)').
+jump_gen_dynamic(X) :-
+    n_property(X,operation),
+    X =.. [P|L],
+    n_findatom(P,operator,A),
+    write('Jcons('),
+    write(A),
+    write(','),
+    jump_gen_dynamic_argument(L),
+    write(',th)').
+jump_gen_dynamic(X) :-
+    n_property(X,userop),
+    functor(X,P,0),
+    write('Jmakeuser("'),
+    write(P),
+    write('")').
+
+jump_gen_dynamic(X) :-
+    n_property(X,userop),
+    X =.. [P|L],
+    write('Jcons(Jmakeuser("'),
+    write(P),
+    write('"),'),
+    jump_gen_dynamic_argument(L),
+    write(',th)').
+jump_gen_dynamic(X) :-
+    n_compiler_variable(X),
+    write('Jmakevar("'),
+    write(X),
+    write('")').
+jump_gen_dynamic(X) :-
+    atom(X),
+	write('Jmakepred("'),
+    write(X),
+    write('")').
+jump_gen_dynamic(X) :-
+    jump_invoke_error('illegal clause ',X).
+
+jump_gen_dynamic_argument([]) :-
+    write('NIL').
+jump_gen_dynamic_argument([X|Xs]) :-
+    write('Jcons('),
+    jump_gen_dynamic(X),
+    write(','),
+    jump_gen_dynamic_argument(Xs),
+    write(',th)').
