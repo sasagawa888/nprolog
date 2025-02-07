3a641c80396d7e2299206259bb08aa18f6a8db8d
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-07 17:23:18 +0900

    update bug.pl

---
+++ tests/bug.pl
@@ -0,0 +1,13 @@
+% test of dynamic/1. see SUPERSET.md
+
+
+:- dynamic(fact/1).
+:- dynamic(baa/1). %ignore
+
+foo(X,Y) :- fact(X,Y).
+
+fact(0,1).
+fact(N,X) :-
+        N1 is N-1,
+        fact(N1,X1),
+        X is N*X1.
