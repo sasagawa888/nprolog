1e91ff84b97674964181fe864dbd0c74e3c62f46
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-07 18:07:51 +0900

    testing compiler dynamic/1

--- tests/bug.pl
+++ tests/bug.pl
@@ -2,7 +2,7 @@
 
 
 :- dynamic(fact/1).
-:- dynamic(baa/1). %ignore
+:- dynamic(bar/1). 
 
 foo(X,Y) :- fact(X,Y).
 
@@ -11,3 +11,8 @@ fact(N,X) :-
         N1 is N-1,
         fact(N1,X1),
         X is N*X1.
+
+bar(1,2).
+bar(a,b).
+bar([1,2,3],a).
+bar(X) :- write($a$).
