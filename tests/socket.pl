9b636e27c12218e5906d904cf3e5890aba32440d
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-09 15:13:35 +0900

    socket.pl add

---
+++ tests/socket.pl
@@ -0,0 +1,19 @@
+% test socket
+
+
+start_server :-
+    create_server_socket(ServerSocket, 5000),
+    writeln('Server started on port 5000'),
+    recv_socket(ServerSocket, Message),
+    write('Server received: '),
+    write(Message),
+    nl,
+    close_socket(ServerSocket).
+
+start_client :-
+    create_client_socket(ClientSocket, 5000, '168.1.1.1'),
+    send_socket(ClientSocket, hello),
+    writeln('Client sent: hello'),
+    close_socket(ClientSocket).
+
+
