c54167af5e76f1514354207b268138bd7c278e52
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-09 16:10:00 +0900

    modify socket.pl

--- tests/socket.pl
+++ tests/socket.pl
@@ -6,14 +6,17 @@ start_server :-
     write('Server started on port 5000'),nl,
     recv_socket(ServerSocket, Message),
     write('Server received: '),
-    write(Message),
-    nl,
+    write(Message),nl,
+    send_socket(ServerSocket, Message),
     close_socket(ServerSocket).
 
 start_client :-
     create_client_socket(ClientSocket, 5000, '102.168.1.1'),
     send_socket(ClientSocket, hello),
     write('Client sent: hello'),nl,
+    recv_socket(ClientSocket, Message),
+    write('Client recv: '),
+    write(Message),nl,
     close_socket(ClientSocket).
 
 
