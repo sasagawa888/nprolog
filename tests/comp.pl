03b2184aa2c603307a6d9d049f9fb82c684404b7
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-17 08:21:47 +0900

    making new analizer

--- tests/comp.pl
+++ tests/comp.pl
@@ -1,14 +1,50 @@
-% about one body
+% idea for new compiler opimizer
 
+% test data
+foo(X) :- write(1)
+
+analize(P) :-
+    n_arity_count(P,[N]),
+	n_clause_with_arity(P,N,C),
+    n_variable_convert(C,C1),
+    analize1(P,C1).
+
+analize1(P,C) :-
+    deterministic(C,0),
+    assert(pred_data(P,det)).
+analize1(P,C) :-
+    tail_recursive(C,0),
+    assert(pred_data(P,tail)).
+
+
+deterministic([],_).
+deterministic([],0) :- fail.
+deterministic([(Head :- Body)|Cs],_) :-
+    det_body(Body),
+    deterministic(Cs,1).
+
+tail_recursive([],_).
+tail_recursive([],0) :- fail.
+tail_recursive([(Head :- Body)|Cs],_) :-
+    tail_body(Head,Body),
+    tail_recursive(Cs,1).
+
+
+unidirectory(Head,Body) :-
+    det_body(Body).
+
+% deterministic body case !
 det_body(!).
 det_body((X,!)).
 det_body((X,(!,Y))) :-
     det_body(Y).
-det_body(X) :- 
-    det_builtin(X).
-det_body((X,Y)) :-
+det_body(_).
+% deterministic body case builtin
+det_body1((X,Y)) :-
     det_builtin(X),
-    det_body(Y).
+    det_body1(Y).
+det_body1(X) :-
+    det_buitlint(X).
 
 det_builtin(write(_)).
 det_builtin(write(_,_)).
@@ -25,3 +61,15 @@ how prove exclusive?
 foo(X) :- X is 1.
 foo(X) :- X is 2.
 */
+
+% tail recursive
+tail_body(Head,Body) :-
+    last_body(Body,Last),
+    functor(Head,Pred1,Arity1),
+    functor(Last,Pred2,Arity2),
+    Pred1 == Pred2,
+    Arity1 == Arity2.
+
+last_body((_,Body),Last) :-
+    last_body(Body,Last).
+last_body(Body,Body).
