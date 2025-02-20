31b9dec0604ce983ec820d1d8f8c3570d2a650ce
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-20 18:26:11 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,4 +1,8 @@
 % testing compile code for diterministic predicate
 
 foo(X) :- X is 3*4.
-
+test3(X,Y)  :- Y is randi(X).
+test4(X)  :- X is random.
+test13(X,Y)  :- Y is integer(X).
+test14(X,Y)  :- Y is abs(X).
+test15(X,Y)  :- Y is sqrt(X).
