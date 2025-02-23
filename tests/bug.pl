4b0f58ff336f6f0ba0aedbbc36a4411f1c55a02a
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-23 18:29:54 +0900

    add todo.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,17 +1,4 @@
 % testing compile code for diterministic predicate
 
-:- dynamic(ack/3).
-% association computation
-
-ack(0,N,A) :- 
-    A is N+1,asserta(ack(0,N,A)).
-ack(M,0,A) :- 
-    M1 is M-1,ack(M1,1,A),
-    asserta(ack(M1,1,A)),
-    asserta(ack(M,0,A)).
-ack(M,N,A) :- 
-    M1 is M-1,N1 is N-1,
-    ack(M,N1,A1), ack(M1,A1,A),
-    asserta(ack(M,N1,A1)),
-    asserta(ack(M1,A1,A)).
-
+foo(0) :- !.
+foo(X) :- write(X),X1 is X-1,foo(X1).
---
+++ tests/todo.pl
@@ -0,0 +1,4 @@
+
+fact(0,1)  without ! is nondet bug?
+
+ack/3 not dynamic compiler is freeze.
