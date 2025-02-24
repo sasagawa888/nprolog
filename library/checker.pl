4daa34be90ebfbee99c19176a06c816538f7513d
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-24 15:55:14 +0900

    update checker

--- library/checker.pl
+++ library/checker.pl
@@ -19,17 +19,11 @@ check :-
 
 
 check_arity(P) :-
-    write('--- arity check ---'),
-    write(P),nl,
     check_arity1(P),!.
 
-check_singleton(P) :-
-    write('--- singleton check ---'),
-    write(P),nl.
+check_singleton(P).
 
-check_callable(P) :-
-    write('--- callable check ---'),
-    write(P),nl.
+check_callable(P).
 
 
 check_arity1(P) :-
@@ -44,33 +38,35 @@ check_arity2(P,[L|Ls]) :-
 
 detect_arity([]).
 detect_arity([(Head :- Body)|Cs]) :-
-    detect_body_arity(Body),
+    detect_body_arity(Head,Body),
     detect_arity(Cs).
 detect_arity([C|Cs]) :-
     detect_arity(Cs).
 
-detect_body_arity((X,Y)) :-
+detect_body_arity(Head,(X,Y)) :-
     n_property(X,predicate),
     functor(X,P,N),
     n_arity_count(P,L),
     member(N,L),
-    detect_body_arity(Y).
-detect_body_arity((X,Y)) :-
+    detect_body_arity(Head,Y).
+detect_body_arity(Head,(X,Y)) :-
     n_property(X,builtin),
     functor(X,P,N),
     system(P/N),
-    detect_body_arity(Y).
-detect_body_arity((X,Y)) :-
-    write('detect '),write(X),nl,
-    detect_body_arity(Y).
-detect_body_arity(X) :-
+    detect_body_arity(Head,Y).
+detect_body_arity(Head,(X,Y)) :-
+    write('detect arity '),write(X),
+    write(' in '),write(Head),nl,
+    detect_body_arity(Head,Y).
+detect_body_arity(Head,X) :-
     n_property(X,predicate),
     functor(X,P,N),
     n_arity_count(P,L),
     member(N,L).
-detect_body_arity(X) :-
+detect_body_arity(Head,X) :-
     n_property(X,builtin),
     functor(X,P,N),
     system(P/N).
-detect_body_arity(X) :-
-    write('detect '),write(X),nl.
+detect_body_arity(Head,X) :-
+    write('detect arity '),write(X),
+    write(' in '),write(Head),nl.
--- tests/bug.pl
+++ tests/bug.pl
@@ -1,4 +1,4 @@
 % testing compile code for diterministic predicate
 
 foo(0) :- !.
-foo(X) :- write(X),X1 is X-1,foo(X1).
+foo(X) :- write(X),X1 is X-1,foo(X1,2).
