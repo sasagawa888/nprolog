576bb7230cbac9e0c372d86b26a428c31d3749f3
sasagawa888 <ken-s@tkcnf.or.jp>
2025-02-28 17:02:02 +0900

    update todo.pl

--- tests/tfact.pl
+++ tests/tfact.pl
@@ -1,6 +1,7 @@
+
 fact(N, X) :- fact_(N, 1, X).
 fact_(N, P, X) :- N > 0,
     N1 is N - 1,
     P1 is P * N,
     fact_(N1, P1, X).
-fact_(0, X, X) :- !.
+fact_(0, X, X).
--- tests/todo.pl
+++ tests/todo.pl
@@ -1,3 +1,3 @@
 For now non.
 
-checker \+ not  arity
+
