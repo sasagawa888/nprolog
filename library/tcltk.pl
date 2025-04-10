e51b0d9954c1f474fcb6728401505ff2759fc1fa
sasagawa888 <ken-s@tkcnf.or.jp>
2025-04-10 09:57:45 +0900

    update tcltk

--- library/tcltk.pl
+++ library/tcltk.pl
@@ -27,68 +27,60 @@ tk_exit :-
     cinline($Tcl_DeleteInterp(interp);
              return(Jexec_all(rest,Jget_sp(th),th));$).
             
-
-
-tk_command_option(X,Y) :-
-    list(X),
-    tk_function(X,Y).
-tk_command_option(X,[Y]) :-
-    format(Y,$ ~A $,X).
-
 tk_label(Obj) :-
     format(Str,$label .~A ~A\n;$,[Obj]),
     tk_interp(Str).
 tk_label(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$label .~A ~A\n;$,[Obj,Opt]),
     tk_interp(Str).
 
 
 tk_button(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$button .~A ~S \n;$,[Obj,Opt]),
     tk_interp(Str).
  
 tk_radiobutton(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$radiobutton .~A ~S\;$,[Obj,Opt]),
     tk_interp(Str).
  
 tk_checkbutton(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$checkbutton .~A ~S\;$,[Obj,Opt]),
     tk_interp(Str).
  
 tk_listbox(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$listbox .~A ~S\;$,[Obj,Opt]),
     tk_interp(Str).
  
 tk_scrollbar(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$scrollbar .~A ~S\;$,[Obj,Opt]),
     tk_interp(Str).
 
 tk_menu(Obj,L) :-
     tk_object(Obj,Objects),
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$menu ~A ~S\n;$,[Objects,Opt]),
     tk_interp(Str).
 
 tk_configure(Obj,L) :-
     tk_object(Obj,Objects),
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$ configure ~A ~S\n;$,[Objects,Opt]),
     tk_interp(Str).
  
 tk_add(Obj,Class,L) :-
     tk_object(Obj,Objects),
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$ add ~A ~S\n;$,[Object,Class,Opt]),
     tk_interp(Str).
 
 tk_canvas(Obj,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$canvas .~A ~S\n;$,[Obj,Opt]),
     tk_interp(Str).
 
@@ -96,8 +88,8 @@ tk_mainloop :-
     cinline($Tk_MainLoop();$).
 
 tk_pack(Obj,L) :-
-    tk_list(Opt),
-    format(Str,$pack ~A \n;$,[Opt]),
+    tk_option(L,Opt),
+    format(Str,$pack ~A ~A \n;$,[Obj,Opt]),
     tk_interp(Str).
 
 
@@ -113,19 +105,19 @@ tk_rgb([R,G,B],Str) :-
     format(Str,$ #~D ~D ~D$,[R,G,B]).
 
 tk_create(Obj,Class,L) :-
-    tk_list(L,Opt),
+    tk_option(L,Opt),
     format(Str,$.~O create ~O Class ~O\n$,[Obj,Class,Opt]),
     tk_interp(Str).
 
-tk_list([],$$).
-tk_list([rgb(R,G,B)|Xs],Str) :-
+tk_option([],$$).
+tk_option([rgb(R,G,B)|Xs],Str) :-
     tk_rgb([R,G,B],S),
     concat($ $,S,S1),
-    tk_list(Xs,S2),
+    tk_option(Xs,S2),
     concat(S1,S2,Str).
 tk_list([X|Xs],Str) :-
     string_term(S,X),
     concat($ $,S,S1),
-    tk_list(Xs,S2),
+    tk_option(Xs,S2),
     concat(S1,S2,Str).
      
