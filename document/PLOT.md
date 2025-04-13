eb263931aa399d866d15d8e8c12b0e37a1945f99
sasagawa888 <ken-s@tkcnf.or.jp>
2025-04-14 08:26:56 +0900

    add plot.pl for GNU-plot

---
+++ document/PLOT.md
@@ -0,0 +1,15 @@
+# Plot library
+under construction
+
+# Preparation
+The Plot library utilizes the C language embedding feature. Therefore, it needs to be compiled first. Please follow the steps below to compile it.
+
+```
+?- use_module(compiler).
+yes
+?- compile_file('./library/plot.pl').
+***
+yes
+```
+
+# Specification
---
+++ library/plot.pl
@@ -0,0 +1,29 @@
+% library for GNU-plot
+
+cdeclare($#include <stdio.h>
+          FILE *gp;$).
+
+/*
+(c-include  "<stdio.h>")
+(c-lang "FILE *gp;")
+
+(defun open-plot ()
+    (c-lang "gp = popen(\"gnuplot -persist\",\"w\");")
+    t)
+
+(defun send-plot (msg)
+     (let ((stm (create-string-output-stream)))
+        (format stm msg)
+        (send-plot1 (get-output-stream-string stm))
+        t))
+
+(defun send-plot1 (msg)
+    (c-lang "fprintf(gp, \"%s\\n\", Fgetname(MSG));")
+    (c-lang "fflush(gp);")
+    t)
+
+(defun close-plot ()    
+    (c-lang "pclose(gp);")
+    t)
+
+*/
