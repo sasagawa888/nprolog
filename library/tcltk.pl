a215ad31e6ceafa246a6fa168e1f1b630cd11062
sasagawa888 <ken-s@tkcnf.or.jp>
2025-04-13 10:18:43 +0900

    update tcltk

--- library/tcltk.pl
+++ library/tcltk.pl
@@ -12,7 +12,7 @@ cdeclare($#define BUFFSIZE 1024
          char buff[BUFFSIZE];
          char subbuff[BUFFSIZE];$).
 
-tk_interp(Str) :-
+tk_interp :-
     cinline($Tcl_Eval(interp,buff);
              return(Jexec_all(rest,Jget_sp(th),th));$).
 
@@ -26,8 +26,6 @@ tk_addint(Int) :-
     cinline($sprintf(subbuff," %d",Jget_int(Jderef(varInt,th)));
              strcat(buff,subbuff);$).
 
-
-
 tk_init :-
     cinline($interp = Tcl_CreateInterp();
              Tcl_Init(interp);               
@@ -37,130 +35,78 @@ tk_init :-
 tk_exit :-
     cinline($Tcl_DeleteInterp(interp);
              return(Jexec_all(rest,Jget_sp(th),th));$).
-            
-tk_label(Obj) :-
-    format(Str,$label .~A ~A\n;$,[Obj]),
-    tk_interp(Str).
-tk_label(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$label .~A ~A\n;$,[Obj,Opt]),
-    tk_interp(Str).
-
-
-tk_button(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$button .~A ~S \n;$,[Obj,Opt]),
-    tk_interp(Str).
  
-tk_radiobutton(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$radiobutton .~A ~S\;$,[Obj,Opt]),
-    tk_interp(Str).
- 
-tk_checkbutton(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$checkbutton .~A ~S\;$,[Obj,Opt]),
-    tk_interp(Str).
- 
-tk_listbox(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$listbox .~A ~S\;$,[Obj,Opt]),
-    tk_interp(Str).
- 
-tk_scrollbar(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$scrollbar .~A ~S\;$,[Obj,Opt]),
-    tk_interp(Str).
-
-tk_menu(Obj,L) :-
-    tk_object(Obj,Objects),
-    tk_option(L,Opt),
-    format(Str,$menu ~A ~S\n;$,[Objects,Opt]),
-    tk_interp(Str).
-
-tk_configure(Obj,L) :-
-    tk_object(Obj,Objects),
-    tk_option(L,Opt),
-    format(Str,$ configure ~A ~S\n;$,[Objects,Opt]),
-    tk_interp(Str).
- 
-tk_add(Obj,Class,L) :-
-    tk_object(Obj,Objects),
-    tk_option(L,Opt),
-    format(Str,$ add ~A ~S\n;$,[Object,Class,Opt]),
-    tk_interp(Str).
-
 tk_canvas(Obj,Opt) :-
     tk_clear,
-    tk_addatom('.'),
+    tk_addatom(' .'),
     tk_addatom(Obj),
     tk_addatom(' canvas'),
     tk_option(Opt),
-    tk_interp(Str).
+    tk_interp.
 
 tk_mainloop :-
     cinline($Tk_MainLoop();$).
 
-tk_pack(Obj,L) :-
-    tk_option(L,Opt),
-    format(Str,$pack .~O ~O \n$,[Obj,Opt]),
-    tk_interp(Str).
+tk_pack(Obj,Opt) :-
+    tk_clear,
+    tk_addatom(' pack .'),
+    tk_addatom(Obj),
+    tk_addatom(' '),
+    tk_option(Opt),
+    tk_interp.
+
 tk_pack(Obj) :-
-    format(Str,$pack .~O \n$,[Obj]),
-    tk_interp(Str).
+    tk_clear,
+    tk_addatom(' pack .'),
+    tk_addatom(Obj),
+    tk_interp.
 
 tk_update :-
     cinline($strcpy(buff,"update\n");
              Tcl_Eval(interp,buff);$).
 
-tk_command(Cmd) :-
-    format(Str,$~O\n;$,[Cmd]),
-    tk_interp(Str).
 
-tk_rgb([R,G,B],Str) :-
-    format(Str,$ #~D ~D ~D$,[R,G,B]).
+tk_rgb([R,G,B]) :-
+    tk_addatom(' #'),
+    tk_addint(R),
+    tk_addatom(' '),
+    tk_addint(G),
+    tk_addatom(' '),
+    tk_addint(B).
+    
 
 tk_create(Obj,Class,Option) :-
-    tk_class(Class,Cls),
-    tk_option(Option,Opt),
-    format(Str,$.~O create ~O ~O\n$,[Obj,Cls,Opt]),
+    tk_clear,
+    tk_addatom('.'),
+    tk_addatom(Obj),
+    tk_addatom(' create '),
+    tk_class(Class),
+    tk_option(Option),
     tk_interp(Str).
 
 tk_class(line(X),Str) :-
-    tk_list(X,S1),
-    format(Str,$-line ~O $,[S1]).
+    tk_addatom(' -line '),
+    tk_intlist(X).
 
+tk_intlist([]).
+tk_intlist([X|Xs]) :-
+    tk_addint(X),
+    tk_intlist(Xs).
 
-tk_list([],$$).
-tk_list([X|Xs],Str) :-
-    string_term(S,X),
-    concat($ $,S,S1),
-    tk_list(Xs,S2),
-    concat(S1,S2,Str).
-
-tk_option([],$$).
-tk_option([rgb(R,G,B)|Xs],Str) :-
-    tk_rgb([R,G,B],S),
-    concat($ $,S,S1),
-    tk_option(Xs,S2),
-    concat(S1,S2,Str).
-tk_option([width(X)|Xs],Str) :-
-    format(S,$ -width ~O $,[X]),
-    concat($ $,S,S1),
-    tk_option(Xs,S2),
-    concat(S1,S2,Str).
-tk_option([height(X)|Xs],Str) :-
-    format(S,$ -height ~O $,[X]),
-    concat($ $,S,S1),
-    tk_option(Xs,S2),
-    concat(S1,S2,Str).
+tk_option([]).
+tk_option([rgb(R,G,B)|Xs]) :-
+    tk_rgb([R,G,B]),
+    tk_option(Xs).
+tk_option([width(X)|Xs]) :-
+    tk_addatom(' -width '),
+    tk_addint(X),
+    tk_option(Xs).
+tk_option([height(X)|Xs]) :-
+    tk_addatom(' -height '),
+    tk_addint(X),
+    tk_option(Xs).
 tk_option([fill(X)|Xs],Str) :-
-    format(S,$ -fill ~A $,[X]),
-    concat($ $,S,S1),
-    tk_option(Xs,S2),
-    concat(S1,S2,Str).
-tk_option([X|Xs],Str) :-
-    string_term(S,X),
-    concat($ $,S,S1),
-    tk_option(Xs,S2),
-    concat(S1,S2,Str).
+    tk_addatom(' -fill '),
+    tk_addatom(X),
+    tk_option(Xs).
+
