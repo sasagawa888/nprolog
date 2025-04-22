e39ba4c862d4857cbc5efdefdab2f95a744eb861
sasagawa888 <ken-s@tkcnf.or.jp>
2025-04-23 07:58:28 +0900

    update clp

--- library/clp.pl
+++ library/clp.pl
@@ -8,7 +8,13 @@
 :- op(600, xfy, #<=).
 :- op(600, xfy, #>=).
 
+
+X #= Y+Z :-
+    integer(X),
+    var(Y),
+    integer(Z),
+    R is X-Z,
+    add_constraint(Y #= R).
 X #= Y :-
     add_constraint(X #= Y).
-
 
