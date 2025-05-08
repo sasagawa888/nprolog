924c3597435b831ecd2aaf1e2dcc5c8a41ebe803
sasagawa888 <ken-s@tkcnf.or.jp>
2025-05-08 12:05:27 +0900

    update clpfd.pl

--- tests/clpfd.pl
+++ tests/clpfd.pl
@@ -53,3 +53,22 @@ test6(X, Y, Z) :-
     X + Y + Z #= 5,
     X #< Z,
     label([X, Y, Z]).
+
+nine_queens(Queens) :-
+    length(Queens, 9),       
+    Queens ins 1..9,         
+    all_different(Queens),   
+    safe(Queens),         
+    label(Queens).
+    
+safe([]).
+safe([Q|Rest]) :-
+    safe(Rest),
+    no_attack(Q, Rest, 1).
+    
+no_attack(_, [], _).
+no_attack(Q, [Q2|Rest], Dist) :-
+    Q #\= Q2 + Dist,  
+    Q #\= Q2 - Dist,
+    Dist1 #= Dist + 1,
+    no_attack(Q, Rest, Dist1).
