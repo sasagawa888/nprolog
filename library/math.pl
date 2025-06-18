98b6e20e88a1378ad9d7913df1a86a170887e7f0
sasagawa888 <ken-s@tkcnf.or.jp>
2025-06-19 08:04:10 +0900

    update math.pl add permutations

--- library/math.pl
+++ library/math.pl
@@ -2,7 +2,8 @@
 :- module(math,[isl/2,union/3,intersection/3,difference/3,subset/2,eqset/2,
                 powerset/2,forall/2,topology/2,topology_space/2,
                 map_val/3,map_set/3,map_compose/3,surjection/3,injection/3,
-                perm_prod/3,perm_inv/3]).
+                list_map/2,map_list/2,map_inv/2,map_list/2,
+                perm_prod/3,perm_inv/2]).
 
 % infix notation
 %:- op(700,xfx,isl).
@@ -118,6 +119,24 @@ map_compose([Y1:X1|Fs],[Y2:X2|Gs],[Y1:Y2|S]) :-
 map_compose([F|Fs],[G|Gs],S) :-
     map_compose(Fs,Gs,S).
 
+list_map(X,Y) :- 
+    list_map1(X,1,Y).
+
+list_map1([],N,[]).
+list_map1([L|Ls],N,[N:L|M]) :-
+    N1 is N+1,
+    list_map1(Ls,N1,M).
+
+
+map_inv([],[]).
+map_inv([X:Y|Ms],[Y:X|R]) :-
+    map_inv(Ms,R).
+
+map_list([],[]).
+map_list([X:Y|Ms],[Y|L]) :-
+    map_list(Ms,L).
+
+
 % surjection([a:1,b:2],[1,2],[a,b]). yes
 surjection(F,S,T) :-
     map_set(F,S,F1),
@@ -145,4 +164,8 @@ nth1(N,[X|Xs],Y) :-
     nth1(N1,Xs,Y).
 
 % inverse of permutation
-
+perm_inv(X,Z) :-
+    list_map(X,M),
+    map_inv(M,Y),
+    sort(Y,Y1),
+    map_list(Y1,Z).
