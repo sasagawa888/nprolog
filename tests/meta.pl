6e66314c62f6507ca670246de05f4c4a62db9b56
sasagawa888 <ken-s@tkcnf.or.jp>
2025-07-05 13:01:21 +0900

    update meta.pl

--- tests/meta.pl
+++ tests/meta.pl
@@ -1,12 +1,24 @@
 % meta interpreter
 
 resolve(true,Env,Cont) :- !.
-resolve((P, Q),Env,Cont) :- !, resolve(P,Env,Cont), resolve(Q,Env,Cont).
-resolve((P; Q),Env,Cont) :- !, resolve(P,Env,Cont) ; resolve(Q,Env,Cont).
-% builtin
+resolve((P, Q),Env,Cont) :- 
+    connect(Q,Cont,Cont1),
+    resolve(P,Env,Cont1).
+resolve((P; Q),Env,Cont) :- 
+    !, resolve(P,Env,Cont) ; resolve(Q,Env,Cont).
+
+% is builtin
+resolve(Goal,Env,Cont) :-
+    functor(Goal,is,2), 
+    arg(1,Goal,X),
+    arg(2,Goal,Y),
+    eval(Y,Y1,Env),
+    unify(X,Y1,Env,Env1),
+    resolve(Cont, Env1, true).
+% other builtin
 resolve(Goal,Env,Cont) :-
     predicate_property(Goal, built_in), !,
-    deref(Goal,Gaol1,Env),
+    deref(Goal,Goal1,Env), 
     call(Goal1),
     resolve(Cont, Env, true).    
 % predicate
@@ -16,16 +28,15 @@ resolve(Head,Env,Cont) :-
 % clause
 resolve(Head,Env,Cont) :-
     functor(Head, F, N),
-    functor(Copy, F, N),
-    clause(Copy, Body),
-    unify(Head, Copy, Env, Env1),
-    append_body(Body,Cont,Body1),
-    resolve(Body1, Env1, Cont).
+    functor(Copy, F, N), 
+    clause(Copy, Body), 
+unify(Head, Copy, Env, Env1),
+    connect(Body,Cont,Body1),
+    resolve(Body1, Env1, true).
 
-
-repl :- 
+prolog :- 
     repeat,
-    nl,write(': ?- '),
+    nl,write('?> '),
     read(X),
     (X=halt -> (get(_),abort);true),
     (resolve(X,[],true) -> write(yes);write(no)),
@@ -85,6 +96,14 @@ unify(X,Y,Env,Env) :-
     findvar(X,X1,Env),
     findvar(Y,Y1,Env),
     X1 == Y1.
+unify(X,Y,Env,[[X,Y]|Env]) :-
+    variable(X),
+    not(variable(Y)),
+    findvar(X,X,Env).
+unify(X,Y,Env,[[Y,X]|Env]) :-
+    not(variable(X)),
+    variable(Y),
+    findvar(Y,Y,Env).
 unify(X,Y,Env,Env) :-
     atom(X),
     atom(Y),
@@ -97,16 +116,54 @@ unify(X,Y,Env,Env1) :-
     list(X),
     list(Y),
     unify1(X,Y,Env,Env1).
-unify(X,Y,Env,Env) :-
+unify(X,Y,Env,Env1) :-
     compound(X),
     compound(Y),
-    deref(X,X1,Env),
-    deref(Y,Y1,Env),
-    X1 == Y1.
+    X =.. X1,
+    Y =.. Y1,
+    unify1(X1,Y1,Env,Env1).
 
 % unify list
+unify1([],[],Env,Env).
 unify1([X|Xs],[Y|Ys],Env,Env2) :-
     unify(X,Y,Env,Env1),
     unify1(Xs,Ys,Env1,Env2).
 
 
+eval(X,X,Env) :-
+    number(X).
+eval(X,X1,Env) :-
+    variable(X),
+    findvar(X,X1,Env).
+eval(X,Y,Env) :-
+    functor(X,'+',2),
+    arg(1,X,X1),
+    arg(2,X,X2),
+    eval(X1,X3,Env),
+    eval(X2,X4,Env),
+    Y is X3+X4.
+eval(X,Y,Env) :-
+    functor(X,'-',2),
+    arg(1,X,X1),
+    arg(2,X,X2),
+    eval(X1,X3,Env),
+    eval(X2,X4,Env),
+    Y is X3-X4.
+eval(X,Y,Env) :-
+    functor(X,'*',2),
+    arg(1,X,X1),
+    arg(2,X,X2),
+    eval(X1,X3,Env),
+    eval(X2,X4,Env),
+    Y is X3*X4.
+eval(X,Y,Env) :-
+    functor(X,'/',2),
+    arg(1,X,X1),
+    arg(2,X,X2),
+    eval(X1,X3,Env),
+    eval(X2,X4,Env),
+    Y is X3/X4.
+
+connect((A,B),C,(A,Z)) :-
+    connect(B,C,Z).
+connect(A,B,(A,B)).
