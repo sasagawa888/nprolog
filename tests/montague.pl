e62b66267fb1559628ce3bdee11b8c85b143a1a8
sasagawa888 <ken-s@tkcnf.or.jp>
2025-07-11 08:01:11 +0900

    add lambda.pl converting from ISLisp

--- tests/montague.pl
+++ tests/montague.pl
@@ -1,11 +1,11 @@
 :- use_module(dcg).
 
 
-% 文 = 名詞句 + 動詞句
+% sentence = noun + verb
 sentence(Sem) -->
     noun_phrase(NP),
     verb_phrase(VP),
-    { Sem = app(NP, VP) }.  % 関数適用の構文木だけ作る
+    { Sem = app(NP, VP) }.  % generate lambda
 
 % every man → λP. ∀x.(man(x) → P(x))
 noun_phrase(lambda(P, forall(X, implies(man(X), app(P, X))))) -->
@@ -14,3 +14,4 @@ noun_phrase(lambda(P, forall(X, implies(man(X), app(P, X))))) -->
 % loves Mary → λx. loves(x, mary)
 verb_phrase(lambda(X, loves(X, mary))) -->
     [loves, mary].
+
