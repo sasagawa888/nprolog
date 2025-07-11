9ab296f4dbe0342995c320131b479dec0087b5f5
sasagawa888 <ken-s@tkcnf.or.jp>
2025-07-11 16:58:40 +0900

    update montague.pl add lambda

--- tests/montague.pl
+++ tests/montague.pl
@@ -1,17 +1,46 @@
 :- use_module(dcg).
 
 
-% sentence = noun + verb
 sentence(Sem) -->
     noun_phrase(NP),
     verb_phrase(VP),
-    { Sem = app(NP, VP) }.  % generate lambda
+    { Sem = [NP, VP] }.
 
-% every man → λP. ∀x.(man(x) → P(x))
-noun_phrase(lambda(P, forall(X, implies(man(X), app(P, X))))) -->
+% every man → lam(P, forall(X, implies(man(X), [P, X])))
+noun_phrase(lam(P, forall(X, implies(man(X), [P, X])))) -->
     [every, man].
 
-% loves Mary → λx. loves(x, mary)
-verb_phrase(lambda(X, loves(X, mary))) -->
+% loves Mary → lam(X, loves(X, mary))
+verb_phrase(lam(X, loves(X, mary))) -->
     [loves, mary].
+
+% ?- phrase(sentence(S), [every, man, loves, mary]).
+
+/*
+* lambda calculas for Montague
+* e.g. ?- reduce([lam(x,x),y],X). X = y .
+*/
+reduce(X,X) :-
+    atom(X).
+reduce(lam(A,B),lam(A,B1)) :-
+    reduce(B,B1).
+reduce([lam(A,B),R],X) :-
+    reduce(R,R1),
+    beta(A,B,R,B1),
+    reduce(B1,X).
+
+beta(A,A,R,R) :-
+  atom(A),
+  atom(R).
+
+beta(A,B,R,X) :-
+  B =.. B1,
+  beta_list(A,B1,R,X1),
+  X =.. X1.
+
+beta_list((A,[],R,[])).
+beta_list(A,[B|Bs],R,[X1|X]) :-
+  beta(A,B,R,X1),
+  beta_list(A,Bs,R,X).
+
 
