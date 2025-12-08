da4d59c07b5550cccbe71dc32c494780decde3ab
sasagawa888 <ken-s@tkcnf.or.jp>
2025-12-08 17:30:41 +0900

    update zfc.pl

--- tests/zfc.pl
+++ tests/zfc.pl
@@ -1,9 +1,59 @@
-:- use_module(math).
+% A set is represented as a list. 
+% Its elements are sorted in ascending order, and no duplicates are allowed.
 
-%Extensionality Axiom
 
-%eqset([1,2,3],[1,2,3]).
+%Axiom of Extensionality
+eqset(A,B) :-
+    A=B.
 
-%Pairing Axiom
+%Axiom of Empty Set
+emptyset([]).
+
+%Axiom of Pairing
 pairset(A,B,P) :-
     sort([A,B],P).
+
+%Axiom of Union
+unionset(A,U) :-
+    union1(A,Flat),
+    sort(Flat,U).
+
+union1([], []).
+union1([Set|Sets], Result) :-
+    append(Set, Rest, Result),
+    union1(Sets, Rest).
+
+%Axiom of Power Set
+powerset([], [[]]).
+powerset([X|Xs], P) :-
+    powerset(Xs, Ps),
+    add_elem(X, Ps, Ps1),
+    append(Ps, Ps1, P).
+
+add_elem(_, [], []).
+add_elem(X, [Set|Sets], [[X|Set]|Sets1]) :-
+    add_elem(X, Sets, Sets1).
+
+%Axiom of Infinity
+%list of prolog is finify
+
+%Axiom Schema of Separation
+separation([],F,[]).
+separation([A|As],F,[A|Bs]) :-
+    P =.. [F,A],
+    call(P),
+    separation(As,F,Bs).
+separation([A|As],F,Bs) :-
+    P =.. [F,A],
+    not(call(P)),
+    separation(As,F,Bs).
+
+%ex　separation([-1,0,1,2],positive,X).
+positive(X) :- 
+    X > 0 .
+
+%Axiom of Choice
+choice([A|As],0,A).
+choice([A|As],N,B) :-
+    N1 is N-1,
+    choice(As,N1,B).
