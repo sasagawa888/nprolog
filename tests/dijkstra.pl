907c193697416c52ca69d50cfd2e454f13648775
sasagawa888 <ken-s@tkcnf.or.jp>
2025-12-21 18:10:10 +0900

    update dijkstra

--- tests/dijkstra.pl
+++ tests/dijkstra.pl
@@ -3,10 +3,9 @@
 :- use_module(graph).
 :- use_module(math).
 
-shortest :-
+shortest(Q) :-
     route(G),
-    dijkstra(G,a,h),
-    listing(q).
+    dijkstra(G,a,h,Q).
 
 
 route(G) :-
@@ -23,18 +22,31 @@ route(G) :-
                     ew(c,f,2),
                     ew(g,h,2)],G).
 
+test0([[a,0,none],[none,100,0]]).
+test1([[b,1,a],[c,7,a],[d,5,a],[a,0,none],[none,100,0]]).
+test2([[e,3,b],[f,5,b],[b,1,a],[c,7,a],[d,2,a],[a,0,none],[none,100,0]]).
 
-test1([[b,1,a],[c,7,a],[d,5,a],[a,0,none],[none,100,a]]).
-test2([[e,3,b],[f,5,b],[b,1,a],[c,7,a],[d,2,a],[a,0,none],[none,100,a]]).
+dijkstra(G,Start,Goal,Q1) :-
+    init(Start,Q),
+    get_shortest(Q,Q,[V,_]),
+    add_candidate(G,Q,Q1),
+    dijkstra1(G,V,Goal,Q1).
 
+dijkstra1(G,Goal,Goal,Q).
+dijkstra1(G,V,Goal,Q) :-
+    write(V),
+    get_shortest(Q,Q,[V,_]),
+    add_candidate(G,Q,Q1),
+    dijkstra1(G,V,Goal,Q1).
 
+% initialize set Q
+init(Start,[[Start,0,none],[none,100,0]]).
 % selected set Q [[vertex,distance,path]...]
 
 % find candidate vertex
 new([V,_,P],[]).
 new([V,_,P],[[_,_,V]|Qs]) :- !,fail.
 new([V,_,P],[_|Qs]) :- new([V,_,P],Qs).
-
 
 
 get_shortest([[V,D,_]],_,[V,D]).
@@ -52,6 +64,7 @@ get_shortest([Q|Qs],A,[V1,D1]) :-
 get_shortest([Q|Qs],A,V) :-
     get_shortest(Qs,A,V).
 
+% add candidate to Q
 add_candidate(G,Q,[[P,D2,V]|Q]) :-
     arg(2,G,Es),
     get_shortest(Q,Q,[V,D]),
