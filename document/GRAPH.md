8137a230136a318ac2ac651a3830855c109aea6a
sasagawa888 <ken-s@tkcnf.or.jp>
2025-12-22 15:18:36 +0900

    update graph lib

--- document/GRAPH.md
+++ document/GRAPH.md
@@ -136,3 +136,26 @@ graph(Vs, Es) is the standard representation used in this library.
 e(U,V) denotes a undirected edge from U to V.
 
 For undirected graphs, treat e(U,V) and e(V,U) as equivalent.
+
+9. dijkstra(G,Start,Goal,Path)
+
+G is undirected-wighted graph
+e.g.
+
+```
+generate_graph([a,b,c,d,e,f,g,h],
+                   [ew(a,b,1),
+                    ew(b,e,2),
+                    ew(b,f,4),
+                    ew(e,f,1),
+                    ew(f,h,6),
+                    ew(a,c,7),
+                    ew(a,d,2),
+                    ew(d,g,5),
+                    ew(c,g,3),
+                    ew(c,f,2),
+                    ew(g,h,2)],G).
+
+```
+![](dijkstra.png)
+
--- library/graph.pl
+++ library/graph.pl
@@ -4,7 +4,7 @@
 %graph([1, 2, 3, 4], [e(1, 2), e(1, 3), e(1, 4), e(2, 3), e(2, 4), e(3, 4)]).
 
 :- module(graph,[graph/2,e/2,ed/2,ew/3,edw/3,generate_graph/3,vertex/2,edge/3,adjacent/3,connected/1,strongly_connected/1,
-                 complete/1,generate_kn/2]).
+                 complete/1,generate_kn/2,dijkstra/4]).
 
 generate_graph(Vs,Es,G) :-
     G = graph(Vs,Es).
@@ -97,3 +97,80 @@ numlist(N,N,[N]).
 numlist(M,N,[M|Ns]) :-
     M1 is M+1,
     numlist(M1,N,Ns).
+
+%------dijkstra shortest-path -----------------
+
+dijkstra(G,Start,Goal,Path) :-
+    init(Start,Q),
+    get_shortest(Q,Q,[V,_]),
+    add_candidate(G,Q,Q1),
+    %write(Q1),nl,
+    dijkstra1(G,Q1,V,Goal,Q2),
+    dijkstra2(Q2,Start,Goal,Path).
+
+dijkstra1(G,Q,Goal,Goal,Q).
+dijkstra1(G,Q,V,Goal,Q2) :-
+    get_shortest(Q,Q,[V1,_]),
+    add_candidate(G,Q,Q1),
+    %write(Q1),nl,
+    dijkstra1(G,Q1,V1,Goal,Q2).
+
+
+dijkstra2([[Start,_,_]|_],Start,Goal,[]).
+dijkstra2([[Goal,_,Prev]|Qs],Start,Goal,[Goal,Prev|Path]) :-
+    dijkstra3(Qs,Prev,[Prev,_,P]),
+    dijkstra2(Qs,Start,P,Path).
+dijkstra2([Q|Qs],Start,Goal,Path) :-
+    dijkstra2(Qs,Start,Goal,Path).
+
+dijkstra3([],Prev,[none,100,_]).
+dijkstra3([[Prev,D,Path]|Qs],Prev,[Prev,D,Path]) :-
+    dijkstra3(Qs,Prev,[Prev,D1,_]),
+    D < D1,!.
+dijkstra3([[Prev,D,Path]|Qs],Prev,[Prev,D1,P]) :-
+    dijkstra3(Qs,Prev,[Prev,D1,P]),
+    D >= D1,!.
+dijkstra3([[Prev,D,Path]|Qs],Prev,[Prev,D,Path]).
+
+dijkstra3([_|Qs],Prev,[Prev,D1,P]) :-
+    dijkstra3(Qs,Prev,[Prev,D1,P]).
+
+
+% initialize set Q
+% max distance is 100.
+init(Start,[[Start,0,none],[none,100,0]]).
+
+% data structure
+% selected set Q [[vertex,distance,path]...]
+% find candidate vertex from Q set.
+new([V,_,P],[]).
+new([V,_,P],[[_,_,V]|Qs]) :- !,fail.
+new([V,_,P],[_|Qs]) :- new([V,_,P],Qs).
+
+get_shortest([[V,D,_]],_,[V,D]).
+get_shortest([Q|Qs],A,[V,D]) :-
+    new(Q,A),
+    [V,D,_] = Q,
+    get_shortest(Qs,A,[V1,D1]),
+    D < D1,!.
+get_shortest([Q|Qs],A,[V1,D1]) :-
+    new(Q,A),
+    [V,D,_] = Q,
+    get_shortest(Qs,A,[V1,D1]),
+    D >= D1,!.
+get_shortest([Q|Qs],A,V) :-
+    get_shortest(Qs,A,V).
+
+% add candidate to Q
+add_candidate(G,Q,Q1) :-
+    arg(2,G,Es),
+    get_shortest(Q,Q,[V,D]),
+    add_candidate1(Q,Es,V,D,Q1).
+
+add_candidate1(Q,[],V,D,Q).
+add_candidate1(Q,[ew(V,P,D1)|Es],V,D,[[P,D2,V]|Qs]) :-
+    D2 is D+D1,
+    add_candidate1(Q,Es,V,D,Qs).
+add_candidate1(Q,[_|Es],V,D,Qs) :-
+    add_candidate1(Q,Es,V,D,Qs).
+    
