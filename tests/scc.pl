ff86654b14e9e757962074b05eca704ba1464bc0
sasagawa888 <ken-s@tkcnf.or.jp>
2025-12-30 10:53:22 +0900

    update scc.pl

--- tests/scc.pl
+++ tests/scc.pl
@@ -1,21 +1,51 @@
 % Strong-connected-component
 
 :- use_module(graph).
+:- use_module(list).
 
-find(V,C) :-
-    generate_graph([1,2,3],[ed(1,2),ed(2,3),ed(3,1),
-                            ed(3,4),ed(4,5),ed(5,6),ed(6,4)],G),
-    scc(G,V,C).
+:- dynamic(component/1).
 
-scc(G,V,C) :-
+find(C) :-
+    abolish(component/1),
+    assert(compnent([])),
+    generate_graph([1,2,3],[ed(1,2),ed(2,3),ed(3,1),ed(3,4),ed(4,1),
+                            ed(4,5),ed(5,6),ed(6,7),ed(7,5)],G),
+    scc(G,C).
+
+scc(G,C) :-
+    arg(1,G,Vs),
     arg(2,G,Es),
-    scc1(G,V,Es,[],C).
+    member(V,Vs),
+    scc1(V,Vs,Es,[]),
+    component(C).
 
-scc1(G,V,[],P,P).
-scc1(G,V,Es,P,P) :-
-    member(V,P).
-scc1(G,V,[ed(V,X)|Es],P,C) :-
-    scc1(G,X,Es,[V|P],C).
-scc1(G,V,[_|Es],P,C) :-
-    scc1(G,V,Es,P,C).
+scc1(S,Vs,Es,P) :-
+    same_set(Vs,P).
 
+scc1(V,Vs,Es,P) :-
+    member(ed(V,X),Es),
+    member(X,P),
+    regist(V,X,P).
+
+scc1(V,Vs,Es,P) :-
+    member(ed(V,X),Es),
+    not(member(X,P)),
+    scc1(X,Vs,Es,[V|P]).
+
+regist(V,X,P) :-
+    regist1(V,X,P,C),
+    component(C),
+    retract(component(C)),
+    assert(component([V|C])).
+
+regist(V,X,P) :-
+    regist1(X,P,C),
+    not(component(C)),
+    assert(component([V|C])).
+
+regist1(X,[X|_],[]).
+regist1(X,[P|Ps],[P|Ys]) :-
+    regist1(X,Ps,Ys).
+
+compile(C) :-
+    setof(X,component(X),C).
