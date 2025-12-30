3f7fb33e5ee81079106b6a5054ecc21c1a6d4f9a
sasagawa888 <ken-s@tkcnf.or.jp>
2025-12-30 13:45:54 +0900

    update scc.pl

--- tests/scc.pl
+++ tests/scc.pl
@@ -2,12 +2,13 @@
 
 :- use_module(graph).
 :- use_module(list).
+:- use_module(math).
 
 :- dynamic(component/1).
 
 find(C) :-
     abolish(component/1),
-    assert(compnent([])),
+    assert(component([])),
     generate_graph([1,2,3],[ed(1,2),ed(2,3),ed(3,1),ed(3,4),ed(4,1),
                             ed(4,5),ed(5,6),ed(6,7),ed(7,5)],G),
     scc(G,C).
@@ -17,10 +18,12 @@ scc(G,C) :-
     arg(2,G,Es),
     member(V,Vs),
     scc1(V,Vs,Es,[]),
-    component(C).
+    fail.
+scc(_,C) :-
+    compile(C).
 
 scc1(S,Vs,Es,P) :-
-    same_set(Vs,P).
+    same_set(Vs,P),!.
 
 scc1(V,Vs,Es,P) :-
     member(ed(V,X),Es),
@@ -32,20 +35,42 @@ scc1(V,Vs,Es,P) :-
     not(member(X,P)),
     scc1(X,Vs,Es,[V|P]).
 
-regist(V,X,P) :-
-    regist1(V,X,P,C),
-    component(C),
-    retract(component(C)),
-    assert(component([V|C])).
 
 regist(V,X,P) :-
     regist1(X,P,C),
-    not(component(C)),
-    assert(component([V|C])).
+    %sort(C,C1),
+    %component(C1),
+    %retract(component(C1)),
+    sort([V|C],C1),
+    assert(component(C1)).
 
-regist1(X,[X|_],[]).
+
+%regist(V,X,P) :-
+%    regist1(X,P,C),
+%    sort([V|C],C1),
+%    not(component(C1)),
+%    assert(component(C1)).
+
+
+regist1(X,[X|_],[X]).
 regist1(X,[P|Ps],[P|Ys]) :-
     regist1(X,Ps,Ys).
 
+
 compile(C) :-
-    setof(X,component(X),C).
+    setof(X,(component(X),X \= []),Y),
+    compile1(Y,C).
+
+compile1([C],[C]).
+compile1([C|Cs],X) :-
+    compile2(C,Cs), % include superset
+    compile1(Cs,X).
+compile1([C|Cs],[C|X]) :-
+    compile1(Cs,X).
+        
+compile2(X,[]) :- fail.
+compile2(X,[Y|Ys]) :-
+    subset(X,Y).
+compile2(X,[Y|Ys]) :-
+    not(subset(X,Y)),
+    compile2(X,Ys).
