059396dc845472dc891c811032b1d272218558bd
sasagawa888 <ken-s@tkcnf.or.jp>
2026-02-04 16:56:36 +0900

    update herbrand

--- tests/herbrand.pl
+++ tests/herbrand.pl
@@ -1,6 +1,15 @@
 % ex Herbrand Thorem
 
-%e.g. forall(x,exist(y,and(p(x),q(y)))).
+%e.g. 
+ex(forall(x,imply(p(x),
+                  exist(y,
+                          and( imply(q(x,y), p(a)),
+                          forall(z, imply(q(y,z), p(x)))
+               ))))).
+
+test(X) :-
+    ex(Y),
+    snf(Y,X).
 
 % 102 is ascii code of atom f
 :- ctr_set(2,102).
@@ -14,6 +23,12 @@ uniquev(X,Y) :-
 uniquef(X) :-
     ctr_inc(2,N),
     char_code(X,N).
+
+skolem(X,Y) :-
+    skolem1(X,[],Y). 
+
+skolem1(X,V,Y) :-
+    foo(X).
 
 alpha(forall(X,F), forall(X1,F1)) :-
     uniquev(X, X1),
@@ -44,20 +59,44 @@ term(X) :-
     compound(X),
     X =.. [P,A],
     member(P,[p,q,r]),
-    member(A,[x,y,z]).
+    member(A,[x,y,z,a,b,c]).
 
-snf(X,X) :-
+
+term(X) :-
+    compound(X),
+    X =.. [P,A1,A2],
+    member(P,[p,q,r]),
+    member(A1,[x,y,z,a,b,c]),
+    member(A2,[x,y,z,a,b,c]).
+
+snf(X,Z) :-
+    snf1(X,Y),
+    alpha(Y,Z).
+
+snf1(X,X) :-
     term(X).
 
-snf(forall(E1,E2),X) :-
-    snf(E2,X).
+snf1(forall(E1,E2),forall(E1,X)) :-
+    snf1(E2,X).
 
-snf(imply(E1,E2),or(neg(X),Y)) :-
-    snf(E1,X),
-    snf(E2,Y).
+snf1(exist(E1,E2),exist(E1,X)) :-
+    snf1(E2,X).
 
-snf(neg(neg(E)),E).
+snf1(and(E1,E2),and(X,Y)) :-
+    snf1(E1,X),
+    snf1(E2,Y).
 
-snf(neg(and(E1,E2)),or(neg(E1),neg(E2))) :-
+
+snf1(or(E1,E2),or(X,Y)) :-
+    snf1(E1,X),
+    snf1(E2,Y).
+
+snf1(imply(E1,E2),or(neg(X),Y)) :-
+    snf1(E1,X),
+    snf1(E2,Y).
+
+snf1(neg(neg(E)),E).
+
+snf1(neg(and(E1,E2)),or(neg(E1),neg(E2))) :-
     term(E1),
     term(E2).
