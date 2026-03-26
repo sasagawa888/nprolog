463b7864c669704243f13458fe43d8072bc6c8b8
sasagawa888 <ken-s@tkcnf.or.jp>
2026-03-27 07:44:00 +0900

    fix plot.pl  add escape for double quote char

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,12 +1,13 @@
-% not tail recursive optimization
 
-:- use_module(utils).
+:- use_module(plot).
 
-output_disp(_S, 0) :- !.
-output_disp(S, N) :-
-    N > 0, !,
-    format(S, $+~D$, [N]).
-output_disp(S, N) :-
-    N < 0,
-    N1 is -N,
-    format(S, $-~D$, [N1]).
+
+foo :-
+    open_plot,
+    send_plot($set title \"sin cos curve\"$),
+    send_plot($set xlabel \"x\"$),
+    send_plot($set ylabel \"y\"$),
+    send_plot($set xtics 1$),
+    send_plot($set ytics 0.5$),
+    send_plot($plot sin(x),cos(x)$),
+    close_plot.
