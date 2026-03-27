e627ce416a1a2a5169cc2962d9cb7b4453c6bcda
sasagawa888 <ken-s@tkcnf.or.jp>
2026-03-27 17:54:18 +0900

    retry TCO from extract_variable

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,2 +1,29 @@
-% no bug data
 
+
+extract_variable(Term,X) :-
+    Term =.. [_|A],
+    extract_variable1(A,X).
+
+extract_variable1([],[]).
+
+extract_variable1([L|Ls],[L|Y]) :-
+    n_compiler_variable(L), !,
+    extract_variable1(Ls,Y).
+extract_variable1([L|Ls],Y) :-
+    atomic(L), !,
+    extract_variable1(Ls,Y).
+extract_variable1([L|Ls],Z) :-
+    list(L), !,
+    extract_variable1(L,Y1),
+    extract_variable1(Ls,Y2),
+    append(Y1,Y2,Z).
+extract_variable1([L|Ls],Z) :-
+    n_property(L,predicate), !,
+    L =.. [_|Args],
+    extract_variable1(Args,Y1),
+    extract_variable1(Ls,Y2),
+    append(Y1,Y2,Z).
+extract_variable1(L,[L]) :-
+    n_compiler_variable(L), !.
+extract_variable1(L,[]) :-
+    atomic(L).
