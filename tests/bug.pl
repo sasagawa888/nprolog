b093f46034c4a2c1e7f62c80ad9e4b212d6cbe3b
sasagawa888 <ken-s@tkcnf.or.jp>
2026-04-02 14:09:38 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,25 +1,8 @@
-% CLP-test  
 
-:- use_module(clpfd).
+% compile error
 
-nqueens(Queens) :-
-    length(Queens, 9),       
-    Queens ins 1..9,         
-    all_different(Queens),   
-    safe(Queens),     
-    label(Queens,trace).
-
-
-safe([]).
-safe([Q|Rest]) :-
-    safe(Rest),
-    no_attack(Q, Rest, 1).
-
-
-no_attack(_, [], _).
-no_attack(Q, [Q2|Rest], Dist) :-
-    Q #\= Q2 + Dist,  
-    Q #\= Q2 - Dist,
-    Dist1 #= Dist + 1,
-    no_attack(Q, Rest, Dist1).
+check_dyn1(X, yes) :-
+    (dyn1(X) -> true ; fail ).
+check_dyn1(X, no) :-
+    \+ dyn1(X).
 
