4260be9c170a5fe2e25312240e492de760858773
sasagawa888 <ken-s@tkcnf.or.jp>
2026-04-03 07:28:54 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,13 +1,17 @@
 
-arc(a1, v1, v2).
-arc(a2, v2, v3).
-arc(a3, v3, v4).
-arc(a4, v4, v1).
-arc(a5, v3, v5).
 
-walk(U, U).
-walk(U, V) :- arc(_, U, U1), walk(U1, V).
+test(A) :-
+    open(S,'./tests/plm1.pl',r),
+    read_codes(S,C),
+    tokenize(C,T),write(T),nl,
+    compile(T,A),
+    close(S).
 
-foo(X,X).
+compile(T,A) :-
+    parse(T,A).
 
-%unify_var() is buggy
+parse(S,A) :-
+    phrase(program(A),S).
+
+program(A) --> pl_module(A).
+program(A) --> statements(A).
