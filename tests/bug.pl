61cbb3c24b103cd49e3df59d1cb40eb42a3b855d
sasagawa888 <ken-s@tkcnf.or.jp>
2026-04-05 09:12:51 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,10 +1,7 @@
-:- dynamic(dyn1/1).
 
-reset_dyn :-
-    retractall(dyn1(_)).
 
-set_dyn :-
-    assertz(dyn1(a)),
-    assertz(dyn1(b)).
-
-foo(_) :- write('ok').
+% ⑧ 強力チェック（同一化バグ検出）
+test_anon6 :-
+    ( (_ = X, _ = Y, X == Y) ->
+        write('OK'), nl
+    ; write('NG'), nl ).
