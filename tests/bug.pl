e18e42f4ede8bd9f51c3dfefe2376ebc2a3179b3
sasagawa888 <ken-s@tkcnf.or.jp>
2026-04-17 07:40:31 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,4 +1,14 @@
 
+case_arg((X;Y),[X1->X5|Z]) :-
+    X = (X1 -> (X2 -> X3 ; X4)),
+    case_arg((X2->X3;X4),L),
+    X5 =.. [case,L],
+    case_arg(Y,Z).
+case_arg((X;Y),[X|Z]) :-
+    case_arg(Y,Z).
+case_arg(X,X).
+
+
 test16 :-
     between(0,5,N),
     (N < 3 ->
