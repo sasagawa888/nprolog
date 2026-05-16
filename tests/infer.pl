fe275fd98e0dcc2bde7c9bc9cc470ea63735b6f0
sasagawa888 <ken-s@tkcnf.or.jp>
2026-05-16 14:25:10 +0900

    udpate infer

--- tests/infer.pl
+++ tests/infer.pl
@@ -40,7 +40,8 @@ test1(P,[N|Ls]) :-
     n_clause_with_arity(P,N,C),
     n_variable_convert(C,C1),
     write('clause = '), write(C1), nl,
-    infer_clause(C1,State,Env,Mode),write(Env),
+    infer_clause(C1,State,Env,Mode),
+    write('Env = '),write(Env),nl,
     test1(P,Ls).
 
 
@@ -48,19 +49,20 @@ test1(P,[N|Ls]) :-
    clause inference
 ========================================= */
 infer_clause([],[],[],Mode).
-infer_clause([C|Cs],State,[E1|Env],Mode) :-
+infer_clause([C|Cs],State, Env ,Mode) :-
     infer_a_clause(C,S1,E1,Mode),
-    infer_clause(Cs,State,Env,Mode).
+    infer_clause(Cs,State,E2,Mode),
+    append(E1,E2,Env).
 
 
-infer_a_clause((Head :- Body), State, [Env1|Env],Mode) :- 
+infer_a_clause((Head :- Body), State, Env1 ,Mode) :- 
     head_args(Head,Args),
-    init_env(Args,Env0),
+    head_env(Args,Env0),
     infer_body(Body,Env0,Env1).
 
 infer_a_clause(Head,State, Env, Mode) :-
     head_args(Head,Args),
-    init_env(Args,Env0).
+    head_env(Args,Env0).
 
 
 head_args(H,Args) :-
@@ -68,25 +70,25 @@ head_args(H,Args) :-
 
 
 
-init_env([],[]).
-init_env([X|Xs],[X|Es]) :-
+head_env([],[]).
+head_env([X|Xs],[X|Es]) :-
     n_compiler_variable(X),
-    init_env(Xs,Es).
-init_env([_|Xs],Es) :-
-    init_env(Xs,Es).
+    head_env(Xs,Es).
+head_env([_|Xs],Es) :-
+    head_env(Xs,Es).
 
 
 
 infer_body(true,Env,Env).
 infer_body((A,B),Env0,Env2) :-
-    infer_goal(A,Env0,Env1),
+    infer_a_body(A,Env0,Env1),
     infer_body(B,Env1,Env2).
 
 infer_body(A,Env0,Env1) :-
     infer_goal(A,Env0,Env1).
 
 
-infer_goal(X,Env0,Env0).
+infer_a_body(X,Env0,Env0).
 
 
 same_struct(X,Y) :-
